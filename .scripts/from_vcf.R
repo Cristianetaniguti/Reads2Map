fe6cd911b583492b4a21e29b602147f5c225712d
Cristianetaniguti <chaytaniguti@gmail.com>
2019-11-29 17:38:19 -0300

    not ready yet

---
+++ .scripts/from_vcf.R
@@ -0,0 +1,387 @@
+# Temporary file: it will be part of empirical.wdl. I am testing separated to save time. 
+
+library(supermassa4onemap)
+library(onemap)
+library(updog)
+library(reshape2)
+library(vcfR)
+library(doParallel)
+library(GUSMap)
+library(ggplot2)
+
+# method_name <- c("freebayes", "gatk")
+# vcf_files <- c("freebayes.recode.vcf", "gatk.recode.vcf")
+# vcf_file <- "gatk_sub.vcf"
+# cross <- "outcross"
+
+# Functions
+create_map_report <- function(input.seq, CountsFrom, SNPCall, GenoCall){
+  if(length(input.seq$seq.num) > 60){
+    size <- 50
+    overlap <- 20
+    around <- 10
+  } else {
+    size <- length(input.seq$seq.num)/2
+    overlap <- size/2
+    around <- size/3
+  }
+  
+  batch_size <- pick_batch_sizes(input.seq,
+                                 size = round(size),
+                                 overlap = round(overlap),
+                                 around = round(around)-1)
+  
+  time_par <- system.time(map_out <- map_overlapping_batches(input.seq, 
+                                                             size = batch_size, phase_cores = 4, overlap = overlap))
+  p <- rf_graph_table(map_out)
+  ggsave(p, filename = paste0(SNPCall, "_", CountsFrom, "_", GenoCall,".png"))
+  
+  times_df <- data.frame(SNPCall,CountsFrom, GenoCall, time_par[3])
+  write_report(times_df, paste0(SNPCall, "_", CountsFrom, "_", GenoCall,"_times.txt"))
+  sizes_df <- data.frame(CountsFrom, SNPCall, GenoCall, "mks" = colnames(map_out$data.name$geno)[map_out$seq.num],
+                         "pos" = map_out$data.name$POS[map_out$seq.num], rf = cumsum(c(0,kosambi(map_out$seq.rf))),
+                         type = map_out$data.name$segr.type[map_out$seq.num], phases = phaseToOPGP_OM(map_out))
+  write_report(sizes_df, paste0(SNPCall, "_", CountsFrom, "_", GenoCall,"_map.txt"))
+}
+
+phaseToOPGP_OM <- function(x){
+  ## code from here taken from the onemap function print.sequence()
+  link.phases <- matrix(NA, length(x$seq.num), 2)
+  link.phases[1, ] <- rep(1, 2)
+  for (i in 1:length(x$seq.phases)) {
+    switch(EXPR = x$seq.phases[i],
+           link.phases[i + 1, ] <- link.phases[i, ] * c(1, 1),
+           link.phases[i +  1, ] <- link.phases[i, ] * c(1, -1),
+           link.phases[i + 1, ] <- link.phases[i, ] * c(-1, 1),
+           link.phases[i + 1, ] <- link.phases[i, ] * c(-1, -1))
+  }
+  if (class(x$data.name)[2] == "outcross") {
+    link.phases <- apply(link.phases, 1, function(x) paste(as.character(x), collapse = "."))
+    parents <- matrix("", length(x$seq.num), 4)
+    for (i in 1:length(x$seq.num)) 
+      parents[i, ] <- onemap:::return_geno(x$data.name$segr.type[x$seq.num[i]], link.phases[i])
+    ## Our code below
+    #transpose the parents and set to baseline
+    parents[which(parents == 'a')] <-'A'
+    parents[which(parents == 'b')] <- 'B'
+    
+    parents = t(parents)
+    if(parents[1,which(apply(parents[1:2,],2,function(x) !(all(x=='A'))))[1]] == 'B')
+      parents[1:2,] <- parents[2:1,]
+    if(parents[3,which(apply(parents[3:4,],2,function(x) !(all(x=='A'))))[1]] == 'B')
+      parents[3:4,] <- parents[4:3,]
+    
+    phases <- GUSMap:::parHapToOPGP(parents)
+    phases[which(phases == 1 | phases == 4)] <- 17 
+    phases[which(phases == 2 | phases == 3)] <- 18
+    phases[which(phases == 5 | phases == 8)] <- 19
+    phases[which(phases == 6 | phases == 7)] <- 20
+    phases[which(phases == 9 | phases == 12)] <- 21
+    phases[which(phases == 10 | phases == 11)] <- 22
+    phases[which(phases == 13 | phases == 16)] <- 23 
+    phases[which(phases == 14 | phases == 15)] <- 24 
+    
+    ## Now from the parental haplotypes, determine the OPGPs
+    return(phases)
+  }
+}
+
+create_filters_report <- function(onemap_obj, CountsFrom, SNPCall, GenoCall) {
+  bins <- onemap::find_bins(onemap_obj)
+  onemap_bins <- create_data_bins(onemap_obj, bins)
+  segr <- onemap::test_segregation(onemap_bins)
+  distorted <- onemap::select_segreg(segr, distorted = T)
+  no_distorted <- onemap::select_segreg(segr, distorted = F, numbers = T)
+  twopts <- rf_2pts(onemap_bins)
+  seq1 <- make_seq(twopts, no_distorted)
+  total_variants <- onemap_obj[[3]]
+  filters_tab <- data.frame(CountsFrom,
+                            SNPCall,
+                            GenoCall,
+                            "n_markers"= total_variants,
+                            "distorted_markers"=length(distorted),
+                            "redundant_markers"=total_variants - length(bins[[1]]))
+  return(list(filters_tab, seq1))
+}
+
+write_report <- function(tab, out_name) {
+  write.table(tab, file=out_name, row.names=F, quote=F)
+}
+
+
+make_vcf <- function(vcf.old, depths, method){
+  # The input od polyRAD need to be a VCF, then this part takes the allele depth from "depths" and put at AD field of input vcf
+  idx <- system(paste0("grep -in 'CHROM' ", vcf.old), intern = T) # This part only works in linux OS
+  idx.i <- strsplit(idx, split = ":")[[1]][1]
+  seed <- sample(1:10000, 1)
+  system(paste0("head -n ", idx.i," ", vcf.old, " > head.",seed))
+  
+  vcf.tab <- read.table(vcf.old, stringsAsFactors = F)
+  
+  if(all(rownames(depths[[1]]) == paste0(vcf.tab[,1], "_", vcf.tab[,2]))){
+    
+    vcf.init <- vcf.tab[,1:8]
+    AD.colum <- rep("AD", dim(vcf.init)[1])
+    vcf.init <- cbind(vcf.init, AD.colum)
+    
+    rs <- rownames(depths[[1]])
+    vcf.init[,3] <- rs
+  } else {
+    temp.tab <- read.table(paste0(method,"_example4ref_alt_alleles.txt"))
+    vcf.init <- cbind(temp.tab[,1:2],paste0(temp.tab[,1], "_", temp.tab[,2]), temp.tab[,3:4],
+                      rep(".", dim(temp.tab)[1]),rep(".", dim(temp.tab)[1]), rep(".", dim(temp.tab)[1]), rep("AD",dim(temp.tab)[1]))
+  }
+  
+  ind.n <- colnames(depths[[1]]) # The names came in different order
+  
+  header <- strsplit(idx, split = "\t")[[1]]
+  ind.vcf <- header[10:length(header)]
+  ind.n <- factor(ind.n, levels = ind.vcf)
+  
+  depths[[1]] <- depths[[1]][,order(ind.n)]
+  depths[[2]] <- depths[[2]][,order(ind.n)]
+  
+  comb.depth <- matrix(paste0(as.matrix(depths[[1]]), ",", as.matrix(depths[[2]])), ncol = ncol(depths[[2]]))
+  colnames(comb.depth) <- ind.vcf
+  #hmc.file <- cbind(rs, comb.depth)
+  
+  vcf.body <- cbind(vcf.init, comb.depth)
+  
+  write.table(vcf.body, file = paste0("temp.body.", seed), quote = FALSE, sep = "\t", row.names = FALSE, col.names = F)
+  
+  system(paste0("cat head.",seed," temp.body.",seed," > temp.",seed,".vcf"))
+  return(paste0("temp.",seed, ".vcf"))
+}
+
+create_gusmap_report <- function(vcf_file, parent1, parent2){
+  ## Maps with gusmap
+  RAfile <- VCFtoRA(vcf_file, makePed = T)
+  
+  ped.file <- read.csv(paste0(method_name,"_ped.csv"))
+  ID <- c(1:(dim(ped.file)[1]))
+  idx.P1 <- which(as.character(ped.file$SampleID) == parent1)
+  idx.P2 <- which(as.character(ped.file$SampleID) == parent2)
+  
+  mother <- c(rep(idx.P1,dim(ped.file)[1]))
+  father <- c(rep(idx.P2,dim(ped.file)[1]))
+  fam <- c(rep("F1", dim(ped.file)[1]))
+  idx.P1 <- which(as.character(ped.file$SampleID) == parent1)
+  idx.P2 <- which(as.character(ped.file$SampleID) == parent2)
+  mother[c(idx.P1, idx.P2)] <- ""
+  father[c(idx.P1, idx.P2)] <- ""
+  fam[c(idx.P1, idx.P2)] <- c("", "")
+  ped.file$IndividualID <- ID
+  ped.file$Mother <- father # Inverted
+  ped.file$Father <- mother
+  ped.file$Family <- fam
+  
+  write.csv(ped.file, file = "ped.file.csv")
+  
+  RAdata <- readRA(paste0(method_name,".recode.vcf.ra.tab"), pedfile = "ped.file.csv", 
+                   filter = list(MAF=0.05, MISS=0.75, BIN=0, DEPTH=0, PVALUE=0.01), sampthres = 0)
+  
+  mydata <- makeFS(RAobj = RAdata, pedfile = "ped.file.csv", 
+                   filter = list(MAF = 0.05, MISS = 0.75,
+                                 BIN = 0, DEPTH = 0, PVALUE = 0.01))
+  
+  
+  pos <- mydata$.__enclos_env__$private$pos
+  depth_Ref_m <- mydata$.__enclos_env__$private$ref[[1]]
+  depth_Alt_m <- mydata$.__enclos_env__$private$alt[[1]]
+  
+  depth_Ref <- list(depth_Ref_m)
+  depth_Alt <- list(depth_Alt_m)
+  config <- mydata$.__enclos_env__$private$config[[1]]
+  
+  phases.gus <- GUSMap:::infer_OPGP_FS(depth_Ref_m, depth_Alt_m, 
+                                       config, epsilon=0.001, reltol=1e-3)
+  
+  rf_est <- GUSMap:::rf_est_FS(init_r = 0.01, ep = 0.001, 
+                               ref = depth_Ref, 
+                               alt = depth_Alt, 
+                               OPGP=list(as.integer(phases.gus)),
+                               nThreads = 1)
+  
+  rf_est$rf[which(rf_est$rf > 0.5)] <- 0.4999999
+  dist.gus <- c(0,cumsum(rf_est$rf))
+  phases.gus[which(phases.gus == 1 | phases.gus == 4)] <- 17
+  phases.gus[which(phases.gus == 2 | phases.gus == 3)] <- 18
+  phases.gus[which(phases.gus == 5 | phases.gus == 8)] <- 19
+  phases.gus[which(phases.gus == 6 | phases.gus == 7)] <- 20
+  phases.gus[which(phases.gus == 9 | phases.gus == 12)] <- 21
+  phases.gus[which(phases.gus == 10 | phases.gus == 11)] <- 22
+  phases.gus[which(phases.gus == 13 | phases.gus == 16)] <- 23
+  phases.gus[which(phases.gus == 14 | phases.gus == 15)] <- 24
+  
+  config[which(config==1)] <- "B3.7"
+  config[which(config==2 | config==3)] <- "D1.10"
+  config[which(config==4 | config==5)] <- "D2.15"
+  
+  map_info <- data.frame("mk.name"= mydata$.__enclos_env__$private$SNP_Names,
+                         "pos" = pos,
+                         "rf" = dist.gus,
+                         "type"= config,
+                         "est.phases"= phases.gus)
+  
+  return(map_info)
+}
+
+
+vcf_file <- "raphael.vcf"
+method_name <- "tassel"
+cross <- "outcross"
+CountsFrom <- "vcf"
+max.cores <- 6
+SNPCall = "tassel"
+parent1 <- "Mal"
+parent2 <- "Sil"
+## READING FINAL VCF FROM PIPELINE
+vcf <- read.vcfR(vcf_file)
+df <- onemap_read_vcfR(vcfR.object=vcf,
+                       cross= cross,
+                       parent1= parent1,
+                       parent2= parent2)
+
+# removing markers with more than 75% of missing data
+df <- filter_missing(df, threshold = 0.25)
+
+# check depths
+create_depths_profile(df, vcf, parent1 = parent1, parent2 = parent2, vcf.par = "AD", 
+                      recovering = FALSE, GTfrom = "vcf", alpha=0.1)
+
+## FILTERS REPORT
+out_name <- paste0(CountsFrom, "_", method_name, "_filters_dfAndGQ.txt")
+filters_tab <- create_filters_report(df, CountsFrom, SNPCall=method_name, GenoCall="df")
+write_report(filters_tab[[1]], out_name)
+
+lgs <- group(filters_tab[[2]])
+lg1 <- make_seq(lgs,1)
+
+create_map_report(input.seq = lg1, CountsFrom = CountsFrom, SNPCall = method_name, GenoCall="df")
+
+# MAPS REPORT - GQ
+aval.gq <- extract_depth(vcfR.object=vcf,
+                         onemap.object=df,
+                         vcf.par="GQ",
+                         parent1=parent1,
+                         parent2=parent2,
+                         recovering=FALSE)
+
+aval.gq <- create_probs(df, genotypes_errors=aval.gq)
+filters_tab <- create_filters_report(aval.gq, CountsFrom, SNPCall=method_name, GenoCall="df")
+
+lgs <- group(filters_tab[[2]])
+lg1 <- make_seq(lgs,1)
+
+create_map_report(lg1, CountsFrom = CountsFrom, SNPCall = method_name, GenoCall="GQ")
+
+# OTHER TOOLS
+## With depths from vcf
+
+updog.aval <- updog_error(
+  vcfR.object=vcf,
+  onemap.object=df,
+  vcf.par="AD",
+  parent1=parent1,
+  parent2=parent2,
+  recovering=TRUE,
+  mean_phred=20,
+  cores=max.cores,
+  depths=NULL)
+
+supermassa.aval <- supermassa4onemap::supermassa_error(
+  vcfR.object=vcf,
+  onemap.object = df,
+  vcf.par = "AD",
+  parent1 = parent1,
+  parent2 = parent2,
+  recovering = TRUE,
+  mean_phred = 20,
+  cores = max.cores,
+  depths = NULL)
+
+polyrad.aval <- polyRAD_error(
+  vcf=vcf_file,
+  onemap.obj=df,
+  parent1=parent1,
+  parent2=parent2,
+  crosstype=cross)
+
+metodologies <- list(updog = updog.aval, supermassa = supermassa.aval, polyrad = polyrad.aval)
+for (metodology in names(metodologies)){
+  cat(metodology, "\n")
+  error_aval <- metodologies[[metodology]]
+  ## Filters
+  out_name <- paste0(method_name, "_filters_", metodology, ".txt")
+  filters_tab <- create_filters_report(error_aval, CountsFrom = CountsFrom, SNPCall = SNPCall, GenoCall = metodology)
+  write_report(filters_tab[[1]], out_name)
+  
+  # Group
+  lgs <- group(filters_tab[[2]])
+  lg1 <- make_seq(lgs,1)
+  
+  ## Maps
+  create_map_report(input.seq = lg1, CountsFrom = CountsFrom, SNPCall = method_name, GenoCall= metodology)
+}
+
+## Depths from bam
+depths.alt <- read.table(paste0(method_name, "_alt_depth_bam.txt"), header = T)
+depths.ref <- read.table(paste0(method_name, "_ref_depth_bam.txt"), header = T)
+
+depths <- list("ref"=depths.ref, "alt"=depths.alt)
+
+updog.aval.bam <- updog_error(
+  vcfR.object=vcf,
+  onemap.object=df,
+  vcf.par="AD",
+  parent1=parent1,
+  parent2=parent2,
+  recovering=TRUE,
+  mean_phred=20,
+  cores=max.cores,
+  depths=depths)
+
+supermassa.aval.bam <- supermassa_error(
+  vcfR.object=vcf,
+  onemap.object = df,
+  vcf.par = "AD",
+  parent1 = parent1,
+  parent2 = parent2,
+  recovering = TRUE,
+  mean_phred = 20,
+  cores = max.cores,
+  depths = depths)
+
+new.vcf <- make_vcf(vcf_file, depths, method_name)
+
+polyrad.aval.bam <- polyRAD_error(
+  vcf=new.vcf,
+  onemap.obj=df,
+  parent1=parent1,
+  parent2=parent2,
+  crosstype=cross)
+
+metodologies <- list(updog = updog.aval.bam, supermassa= supermassa.aval.bam, polyrad=polyrad.aval.bam)
+for (metodology in names(metodologies)){
+  error_aval <- metodologies[[metodology]]
+  ## Filters
+  out_name <- paste0(method_name, "_filters_bam_", metodology, ".txt")
+  filters_tab <- create_filters_report(error_aval)
+  write_report(filters_tab, out_name)
+  
+  # Group
+  lgs <- group(filters_tab[[2]])
+  lg1 <- make_seq(lgs,1)
+  
+  ## Maps
+  create_map_report(input.seq = lg1, CountsFrom = CountsFrom, SNPCall = method_name, GenoCall= metodology)
+}
+
+## Gusmap maps
+out_name <- paste0(method_name, "_map_gusmap.txt")
+map_gus <- create_gusmap_report(vcf_file)
+write_report(map_gus, out_name)
+
+out_name <- paste0(method_name, "_map_bam_gusmap.txt")
+map_gus <- create_gusmap_report(new.vcf)
+write_report(map_gus, out_name)
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-## Linkage map simulation
+## Building linkage maps with onemap_ht
 
-This workflow provides linkage map simulation using markers from RAD-seq methodology. The main goal of the workflow is to measuare the impact of different error probabilities coming from SNP calling methodologies in the linkage map building in OneMap.
+Here you will find workflows written in WDL to perfom linkage map building from simulated and empirical data. The workflow main.wdl provides linkage map simulation using markers from RAD-seq methodology.The workflow empirical.wdl receives fastq files and perform all procedure until the genetic maps. The main goal of the workflow is to measuare the impact of different error probabilities coming from SNP calling methodologies in the linkage map building in OneMap.
 
 ## Quickstart
 
 This workflow requires docker hub images. First of all, install [docker](https://docs.docker.com/install/) and [cromwell](https://cromwell.readthedocs.io/en/stable/tutorials/FiveMinuteIntro/).
 
-### Run for only one family
+### Run the simulations
 
 * Adapt the path of the inputs in `/main.inputs.json`
 
@@ -31,6 +31,73 @@ This workflow requires docker hub images. First of all, install [docker](https:/
 - doses: file containing the percentage of markers with doses 0, 1 and 2 (when cross is F1)
 - ploidy: the ploidy of the specie, by now only diploid (2) species are supported
 - cross: cross type: "F1" or "F2"
+
+
+### Run with empirical data
+
+* Adapt the path of the inputs in `empirical.json`
+
+*empirical.references*
+- ref_fasta: chromosome sequence in fasta format (only one chromosome at a time) 
+- ref_fasta_index: index made by samtools faidx
+- ref_dict: index made by picard dict
+- ref_sa: index made by bwa index
+- ref_amb: index made by bwa index
+- ref_bwt: index made by bwa index
+- ref_ann: index made by bwa index
+- ref_pac: index made by bwa index
+
+*empirical.dataset*
+- samples_info: tsv file with first column with path to fastq file, second column with sample names and third column with sample names and lane specifications.
+
+- name: specie name
+
+#### Test dataset
+
+You can download black cottonwood genome assembly (FASTA) and RADseq reads (two FASTQs) from NCBI for testing.
+
+```
+# Download a subset of RADseq data from populus study using SRA toolkit (https://www.ncbi.nlm.nih.gov/sra/docs/toolkitsoft/)
+
+for i in SRR6249795 SRR6249808 SRR6249768 SRR6249769 SRR6249770 SRR6249771 SRR6249772 SRR6249773 SRR6249774 SRR6249775 SRR6249776 SRR6249778 SRR6249779 SRR6249780 SRR6249781 SRR6249782 SRR6249783 SRR6249784 SRR6249785 SRR6249786 SRR6249787 SRR6249788; do
+    ./fasterq-dump $i
+    head -n 1200000 $i.fastq > $i.sub.fastq # Just a subset of the reads
+done
+
+# Here there are data enought to test the pipeline but not for having a good resolution genetic map. It contains the two parents and 20 progeny individuals. The original study have eight replicates for each parent and 122 progenies.
+# Access all samples in the BioProject	PRJNA395596
+
+# Download the reference genome in https://phytozome.jgi.doe.gov/pz/portal.html#!bulk?org=Org_Ptrichocarpa
+# Here we only use chromosome 10
+```
+
+samples_info
+
+```
+PT_F.sub.fastq	PT_F	PT_F.Lib1_E09_TGAACAT
+PT_M.sub.fastq	PT_M	PT_M.Lib2_E06_CGATGCG 
+II_3_39.sub.fastq II_3_39 II_3_39.Lib2_B08_TAGGCGC
+II_3_40.sub.fastq II_3_40 II_3_40.Lib2_B09_GGAACTG
+II_3_29.sub.fastq II_3_29 II_3_29.Lib1_A01_AACAATG
+II_3_31.sub.fastq II_3_31 II_3_31.Lib2_B07_ATCTGTT
+II_3_27.sub.fastq II_3_27 II_3_27.Lib2_B05_GGAGACT
+II_3_28.sub.fastq II_3_28 II_3_28.Lib2_B06_CCTCTAG
+II_3_25.sub.fastq II_3_25 II_3_25.Lib2_B03_ACTTCTG
+II_3_26.sub.fastq II_3_26 II_3_26.Lib2_B04_TTGAGGC
+II_3_43.sub.fastq II_3_43 II_3_43.Lib2_B11_TTCTGAG
+I_3_38.sub.fastq I_3_38 I_3_38.Lib1_B10_CCTCAGC
+I_3_34.sub.fastq I_3_34 I_3_34.Lib1_B08_TAGGCGC
+I_3_53.sub.fastq I_3_53 I_3_53.Lib1_C07_TTCTAGT
+I_3_50.sub.fastq I_3_50 I_3_50.Lib1_C06_CCTAGAT
+I_3_45.sub.fastq I_3_45 I_3_45.Lib1_C03_ATGCCGG
+I_3_42.sub.fastq I_3_42 I_3_42.Lib1_C02_CCTGACT
+I_3_59.sub.fastq I_3_59 I_3_59.Lib1_C12_ACGTTGT
+I_3_58.sub.fastq I_3_58 I_3_58.Lib1_C11_TTCCACG
+I_3_56.sub.fastq I_3_56 I_3_56.Lib1_C10_CCTGCAC
+I_3_55.sub.fastq I_3_55 I_3_55.Lib1_C09_AGAAGTC
+I_3_66.sub.fastq I_3_66 I_3_66.Lib1_D06_GCCAACT
+```
+
 
 ### WDL configurations
 
