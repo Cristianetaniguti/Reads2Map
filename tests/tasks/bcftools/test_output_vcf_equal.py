dd75b72f2c53c7a3144ae8dcbb354ba2d36426de
Lucas Taniguti <lucas.taniguti@mendelics.com.br>
2022-11-19 14:02:49 -0300

    tests: add class to check if two VCF files are equal

--- tests/tasks/bcftools/test_output_vcf_equal.py
+++ tests/tasks/bcftools/test_output_vcf_equal.py
@@ -1,23 +1,12 @@
-# from pathlib import Path
+from pathlib import Path
 
-# import pytest
-
-# from utils.vcf import assert_vcf_equal
+import pytest
 
 
-# @pytest.mark.workflow("Use bcftools view to create bgzip and tabixed vcfs from the provided VCF inputs")
-# def test_ensure_vcf_1_are_equal(workflow_dir):
-#     vcf1 = Path(workflow_dir, "_LAST/out/vcf_files/0/TEST-ONLY.another-test.conversion.vcf.gz")
-#     vcf2 = Path("cached-data/circleci-test-data/utils_tests/expected_left_align_vcf.vcf.gz")
+@pytest.mark.workflow("Use bcftools to normalize variant representation in VCF")
+def test_ensure_vcf_1_are_equal(workflow_dir, compare_files):
+    vcf1 = Path(workflow_dir, "_LAST/out/vcf_norm/vcf_norm.vcf.gz")
+    vcf2 = Path("tests/data/ecoli/ecoli_first_10k_bases.normalized.vcf.gz")
 
-#     # Asserts
-#     assert assert_vcf_equal(vcf1, vcf2)
-
-
-# @pytest.mark.workflow("Use bcftools view to create bgzip and tabixed vcfs from the provided VCF inputs")
-# def test_ensure_vcf_2_are_equal(workflow_dir):
-#     vcf1 = Path(workflow_dir, "_LAST/out/vcf_files/1/TEST-ONLY.testing.conversion.vcf.gz")
-#     vcf2 = Path("cached-data/circleci-test-data/utils_tests/mito_variants_other.vcf.gz")
-
-#     # Asserts
-#     assert assert_vcf_equal(vcf1, vcf2)
+    # Asserts
+    assert compare_files.assert_vcf_equal(vcf1, vcf2)
